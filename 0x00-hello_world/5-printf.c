a1a7c9a2a406b0b4ab8865569239bf275196c51e
Rode1o <dumirola.dr@gmail.com>
2021-02-03 16:57:12 -0800

    ADD C programs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Where the program start its execution
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
