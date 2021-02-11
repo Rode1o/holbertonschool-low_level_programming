10ddae36c702e124bf02c574c0698251bc8f9f37
Duvan Rodelo <dumirola.dr@gmail.com>
2021-02-10 16:26:54 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This program prints Holberton, followed by a new line.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+int a = 1, b = 2, c, i;
+for (i = 1; i <= 50; i++)
+{
+printf("%d\n", a);
+c = a + b;
+a = b;
+b = c;
+}
+return (0);
+}
