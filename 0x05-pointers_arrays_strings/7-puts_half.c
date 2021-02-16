23acb7fd7aaee220838b50079c4df15e9611c9d5
Duvan Rodelo <dumirola.dr@gmail.com>
2021-02-15 20:40:52 -0800

    pointers & arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * puts_half - main function.
+ * @str: Pointer, the string of numbers to print.
+ *
+ * Description: This function prints half of a string.
+ *
+ * Return: none.
+ */
+
+void puts_half(char *str)
+{
+	int n;
+	int i;
+
+	while (str[n])
+	{
+		n++;
+	}
+	if (n % 2 == 1)
+	{
+		i = (n - 1) / 2;
+		i = n - i;
+	}
+	else
+	{
+		i = n / 2;
+	}
+
+
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
