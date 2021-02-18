9df9aa0f07d1060f6b3724c7daba2b76a5e53d8b
Duvan Rodelo <dumirola.dr@gmail.com>
2021-02-18 14:00:41 -0800

    Strncat

---
+++ 0x06-pointers_arrays_strings/1-strnchat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+  * _strncat - Concatenates two strings
+  * @dest: destination string
+  * @src: source string
+  * @n: number of characters
+  * Return: a pointer to the resulting string dest
+**/
+char *_strncat(char *dest, char *src, int n)
+{
+	int d1, d2;
+
+	for (d1 = 0; dest[d1] != '\0'; d1++)
+	{
+	}
+	for (d2 = 0; d2 < n; d2++)
+	{
+		dest[s1] = src[s2];
+		s1++;
+	}
+	return (dest);
+}
