dab7152f21e726ead190d4b0ffde1f14025bd7d6
Duvan Rodelo <dumirola.dr@gmail.com>
2021-02-18 15:41:21 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,12 +7,13 @@
 **/
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s1[i] == s2[i]; i++)
-	
+	while (s1[i] == s2[i])
+	{
+		i++;
 		if (s1[i] == '\0')
 			return (0);
-	
+	}
 	return (s1[i] != s2[i]);
 }
