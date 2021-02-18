5ff9504b6a6045a05916edfd2c6fa150d53b614f
Duvan Rodelo <dumirola.dr@gmail.com>
2021-02-18 15:24:01 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+  * _strcmp - compares two strings
+  * @s1: string 1
+  * @s2: string 2
+  * Return: integer
+**/
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		i++;
+		if (s1[i] == '\0')
+			return (0);
+	}
+	return (s1[i] != s2[i]);
+}
