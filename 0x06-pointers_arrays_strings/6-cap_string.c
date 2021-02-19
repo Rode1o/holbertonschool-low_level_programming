ffd3a8deaf7790f91d4e89cde6f482929a5b8d92
Duvan Rodelo <dumirola.dr@gmail.com>
2021-02-18 18:50:39 -0800

    Capitalize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,15 +17,11 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; x[j] != '\0'; j++)
-//if (s[i] == x[j])
-//{
-//    i++;
 			if (s[i] == x[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			{
 				s[i + 1] = s[i + 1] - 32;
 				break;
 			}
-//}
 	}
 	return (s);
 }
