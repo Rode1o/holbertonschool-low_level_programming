eb2bdda3e00984e76419f48181e3cf4e6d2095dd
Duvan Rodelo <dumirola.dr@gmail.com>
2021-02-18 18:48:43 -0800

    Capitalize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,31 @@
 #include "holberton.h"
 /**
-  * cap_string - capitalize all words of a string
-  * @s: string
-  * Return: pointer to string s
-**/
+ * cap_string - Capitalizes all words of a string.
+ *
+ * @s: String
+ *
+ * Return: Capitalized string
+ */
 char *cap_string(char *s)
 {
-int i = 0, j = 0;
-char sep [] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t'};
-if (s[0] >= 'a' && s[0] <= 'z')
-	s[0] = s[0] - 32;
-while (s[i] != '\0')
-{
-while (sep[j] != '\0')
-{
-if (s[i] == sep[j] && s[i] >= 'a' && s[i] <= 'z')
-{
-s[i] -= 32;
-}
-j++;
-}
-i++;
-}
-return (s);
+	int i, j;
+
+	char x[] = {'}', ')', '?', '!', '"', '.', '\n',
+		    '\t', ',', ';', ' ', '(', '{'};
+	if (x[0] >= 'a' && x[0] <= 'z')
+		s[0] = s[0] - 32;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; x[j] != '\0'; j++)
+//if (s[i] == x[j])
+//{
+//    i++;
+			if (s[i] == x[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+				break;
+			}
+//}
+	}
+	return (s);
 }
