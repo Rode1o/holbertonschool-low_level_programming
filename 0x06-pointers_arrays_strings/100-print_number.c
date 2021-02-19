921ad8edd3b40831bb58de3d124060486864f3d7
Duvan Rodelo <dumirola.dr@gmail.com>
2021-02-19 09:29:10 -0800

    Fix spaces

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -11,13 +11,13 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		x = -n;
 	}
 	else
 		x = n;
-	if (n / 10)
+	if (x / 10)
 	{
-		print_number(n / 10);
+		print_number(x / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(x % 10 + '0');
 }
