441b850c204791fab4db53cf77a81aff202ff32a
Duvan Rodelo <dumirola.dr@gmail.com>
2021-03-11 20:53:47 -0800

    helloHolberton

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -2,7 +2,7 @@ section .data                           ;.data starts here
 	msg db 10,"Hello, Holberton"	;String gets initialized
 	l equ $-msg			;Length Of String
 section .text				;.text starts here
-	global main			;Moving to _start
+global main			;Moving to _start
 main:					;_start label
 	mov rax,1			;Sys_Write Function
 	mov rdi,1			;Std_Out File Descriptor
