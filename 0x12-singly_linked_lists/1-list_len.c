a756bee9db470c25fca4564eae6e6fa11616b554
Rode1o <dumirola.dr@gmail.com>
2021-03-19 16:24:34 -0700

    Exercise 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: list to count the number of elements in
+ * Return: number of elements in the linked list provided
+ */
+size_t list_len(const list_t *h)
+{
+	size_t token = 0;
+
+	while (h)
+	{
+		token++;
+		h = h->next;
+	}
+
+	return (token);
+}
