67c22242a78bcea2fb823eeda655a9b5578216ff
Rode1o <dumirola.dr@gmail.com>
2021-03-23 07:35:03 -0700

    Fix structure

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,10 +17,10 @@
 typedef unsigned int UINT;
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-}list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
