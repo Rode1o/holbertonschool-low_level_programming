dc16a95788428b34179f810ad7b2ad1e2e578e77
Rode1o <dumirola.dr@gmail.com>
2021-03-29 14:58:55 -0700

    fix loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
 
 /**
- * create_file - function to create a file
- * @filename: name of file to create
- * @text_content: string to write to file
- * Return: 1 for success and -1 for failure
+ * create_file - main function
+ * @filename: The pointer name of the file.
+ * @text_content: The string.
+ *
+ * Description: This function creates a file.
+ *
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
