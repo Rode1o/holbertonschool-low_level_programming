aeda665b59700a8f06983c7c802f2b8794631e87
Rode1o <dumirola.dr@gmail.com>
2021-05-27 22:28:04 -0700

    condtional null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,16 +10,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht = NULL;
 	unsigned long int token;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
+	if (size > 0)
+	{
+		ht = malloc(sizeof(hash_table_t));
+		if (!ht)
+			return (NULL);
+		ht->size = size;
+		ht->array = malloc(sizeof(hash_node_t *) * size);
+		if (!ht->array)
+			free(ht);
 		return (NULL);
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (!ht->array)
-		free(ht);
-		return (NULL);
-	for (token = 0; token < size; token++)
-		ht->array[token] = NULL;
+		for (token = 0; token < size; token++)
+			ht->array[token] = NULL;
 
-	return (ht);
+		return (ht);
+	}
+	return (NULL);
 }
