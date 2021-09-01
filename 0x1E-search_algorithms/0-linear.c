d1fad142147667103a2c6456e117aaecc8b6387e
Rode1o <dumirola.dr@gmail.com>
2021-09-01 05:29:36 -0500

    betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,10 +9,9 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
-
-	if (!array || size == 0)
-		return (-1);
+size_t i = 0;
+if (!array || size == 0)
+return (-1);
 for (i = 0; i < size; i++)
 {
 printf("Value checked array[%ld] = [%d]\n", i, array[i]);
